fde2912cfc10a8514ef5d1dd50e3a41498fa5e58
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-12-14 20:15:50 +0200

    smallest-number

---
+++ python-fundamentals/functions/smallest-number.py
@@ -0,0 +1,9 @@
+def smallest_number(num_one, num_two, num_three):
+    return min(num_one, num_two, num_three)
+
+
+first_integer = int(input())
+second_integer = int(input())
+third_integer = int(input())
+
+print(smallest_number(first_integer, second_integer, third_integer))
